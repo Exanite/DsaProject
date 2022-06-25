4696cc7074b29bff2ae0e00384e700598c379f88
Exanite <exanite78012@gmail.com>
2022-06-25 18:00:45 -0400

    Rename views folder to pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,12 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomePage from '../pages/HomePage.vue'
+import AboutPage from '../pages/AboutPage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomePage
   },
   {
     path: '/about',
@@ -13,7 +14,7 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutPage
   }
 ]
 
