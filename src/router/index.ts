e04b812255e8c9cbf56e6b7156b345e177282063
Exanite <exanite78012@gmail.com>
2022-06-25 18:03:23 -0400

    Change imports to be absolute

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,26 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import HomePage from '../pages/HomePage.vue'
-import AboutPage from '../pages/AboutPage.vue'
+import AboutPage from "@/pages/AboutPage.vue";
+import HomePage from "@/pages/HomePage.vue";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'home',
-    component: HomePage
+    path: "/",
+    name: "home",
+    component: HomePage,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/about",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: AboutPage
-  }
-]
+    component: AboutPage,
+  },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
