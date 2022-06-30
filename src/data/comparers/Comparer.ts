f10243ec735a8e8b620617a41e078bbd12df91bb
Exanite <exanite78012@gmail.com>
2022-06-30 01:55:54 -0400

    Update jsdoc for Comparer

--- src/data/comparers/Comparer.ts
+++ src/data/comparers/Comparer.ts
@@ -1,11 +1,12 @@
 /**
- * Generic way to compare an object's properties
- * Traverses an object using the given path and compares the properties found
+ * Generic way to compare an object's properties by traversing an
+ * object using the given path and compares the properties found.
  * 
- * Note: Providing an empty path compares the root objects
+ * Note: Providing an empty path compares the root objects.
  * 
  * @example
  * const comparer = new Comparer(["totals", "capitalOutlay"]);
+ * comparer.compare(a, b);
  */
 export class Comparer {
   constructor(
