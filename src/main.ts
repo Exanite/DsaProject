9bed5fc3fe9445d15ea5ec491d6ec42381b91854
Exanite <exanite78012@gmail.com>
2022-07-17 15:13:17 -0400

    Remove debug code in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
 import App from "@/App.vue";
-import { BuiltInSortingStrategy } from "@/data/algorithms/BuiltInSortingStrategy";
-import { Comparer } from "@/data/comparers/Comparer";
-import { DataGenerator } from "@/data/DataGenerator";
 import router from "@/router";
 import store from "@/store";
 import "@/styles/globals.css";
@@ -11,13 +8,3 @@ createApp(App)
   .use(store)
   .use(router)
   .mount("#app");
-
-// Todo Remove
-const generator = new DataGenerator();
-const comparer = new Comparer(["year"], true);
-
-const collection = generator.generateCollection(100);
-
-const sorted = BuiltInSortingStrategy.sort(collection, comparer);
-
-console.log(sorted);
