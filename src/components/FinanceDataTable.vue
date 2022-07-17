54238008bcfd57e6175a6569bc19fa3d7858fe9a
Exanite <exanite78012@gmail.com>
2022-07-17 14:31:34 -0400

    Add FinanceDataTable

---
+++ src/components/FinanceDataTable.vue
@@ -0,0 +1,25 @@
+<template>
+  <p>
+    {{ JSON.stringify(data)}}
+  </p>
+</template>
+
+<script lang="ts">
+  import { FinanceDataEntry } from "@/data/FinanceData";
+  import { defineComponent } from "vue";
+
+  export default defineComponent({
+    name: "FinanceDataTable",
+    props: {
+      data: {
+        type: Array as () => FinanceDataEntry[],
+        required: true,
+      }
+    },
+    setup(props) {
+      return {
+        data: props.data,
+      }
+    }
+  });
+</script>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,23 +1,32 @@
 <template>
   <div class="home">
     <nav>
-    <router-link to="/">Home</router-link>
-    |
-    <router-link to="/about">About</router-link>
-  </nav>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+      <router-link to="/">Home</router-link>
+      |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <FinanceDataTable :data="financeData"/>
   </div>
 </template>
 
 <script lang="ts">
+  import FinanceDataTable from "@/components/FinanceDataTable.vue";
   import HelloWorld from "@/components/HelloWorld.vue";
-  import { defineComponent } from "vue";
-  
+  import { getFinanceData } from "@/data/FinanceData";
+  import { defineComponent, ref } from "vue";
+
   export default defineComponent({
     name: "HomePage",
     components: {
+      FinanceDataTable,
       HelloWorld,
     },
+    setup() {
+      const financeData = ref(getFinanceData());
+      
+      return {
+        financeData: financeData,
+      }
+    }
   });
 </script>
