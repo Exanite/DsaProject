17f41a48a58cbf189ef224542498553b822e0df4
Exanite <exanite78012@gmail.com>
2022-07-17 01:03:12 -0400

    Adjust BuiltInSortingStrategy name

--- src/data/algorithms/BuiltInSortingStrategy.ts
+++ src/data/algorithms/BuiltInSortingStrategy.ts
@@ -2,7 +2,7 @@ import { SortingStrategy } from "@/data/algorithms/SortingStrategy";
 import { Comparer } from "@/data/comparers/Comparer";
 
 export const BuiltInSortingStrategy: SortingStrategy = {
-  name: "Built In JS Sort",
+  name: "JS Built In Sort",
 
   sort<T>(collection: T[], comparer: Comparer): T[] {
     return collection.sort(comparer.compare.bind(comparer));
