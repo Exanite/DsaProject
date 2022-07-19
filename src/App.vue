091c334847dada887621ecb53df31afc70b71c8c
Exanite <exanite78012@gmail.com>
2022-07-18 21:20:14 -0400

    Clean up copied code

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,6 @@
 <template>
   <div>
     <div>
-      <!-- Mobile -->
-      <div aria-modal="true" class="relative z-40 md:hidden" role="dialog">
-        <div class="fixed inset-0 bg-gray-600 bg-opacity-75"></div>
-
-        <div class="fixed inset-0 flex z-40">
-          <div class="relative flex-1 flex flex-col max-w-xs w-full bg-gray-800">
-            <div class="absolute top-0 right-0 -mr-12 pt-2">
-              <button
-                class="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                type="button"
-              >
-                <span class="sr-only">Close sidebar</span>
-                <svg
-                  aria-hidden="true"
-                  class="h-6 w-6 text-white"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M6 18L18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
-              </button>
-            </div>
-
-            <div class="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
-              <div class="flex-shrink-0 flex items-center px-4">
-                <!-- <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow"> -->
-                Hello World
-              </div>
-              <nav class="mt-5 px-2 space-y-1">
-                <a
-                  v-for="item in nav"
-                  :key="item.name"
-                  :href="item.href"
-                  class="bg-gray-900 text-white group flex items-center px-2 py-2 text-base font-medium rounded-md"
-                >
-                  <svg
-                    aria-hidden="true"
-                    class="text-gray-300 mr-4 flex-shrink-0 h-6 w-6"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path :d="item.icon" stroke-linecap="round" stroke-linejoin="round"/>
-                  </svg>
-                  {{ item.name }}
-                </a>
-
-              </nav>
-            </div>
-          </div>
-
-          <div class="flex-shrink-0 w-14">
-          </div>
-        </div>
-      </div>
-
-      <!-- Desktop -->
       <div class="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
         <div class="flex-1 flex flex-col min-h-0 bg-gray-800">
           <div class="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
@@ -70,7 +8,7 @@
               <!-- <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow"> -->
               Home
             </div>
-            <nav class="flex-1 px-2 space-y-1">
+            <nav class="flex-1 px-2 space-y-1 pt-8">
               <a
                 v-for="item in nav"
                 :key="item.name"
@@ -119,27 +57,46 @@
         <main class="flex-1">
           <div class="py-6">
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              <h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
+              <h1 class="text-center text-2xl font-semibold text-gray-900">Dashboard</h1>
             </div>
             <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+              <div class="py-4"></div>
               <router-view/>
-              <div class="py-4">
-                <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
-              </div>
             </div>
           </div>
         </main>
       </div>
     </div>
-
-
   </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from "vue";
+  import { defineComponent, ref } from "vue";
 
   export default defineComponent({
     name: "App",
+    setup() {
+      const navigation = ref([
+        {
+          name: "Home",
+          href: "#",
+          icon: "M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6",
+        },
+        {
+          name: "Page 1",
+          href: "#/about",
+          icon: "M8 12h.01M12 12h.01M16 12h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z",
+        },
+        {
+          name: "Page 2",
+          href: "#/about",
+          icon: "M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z",
+        },
+      ]);
+
+      return {
+        nav: navigation,
+      };
+    },
   });
 </script>
