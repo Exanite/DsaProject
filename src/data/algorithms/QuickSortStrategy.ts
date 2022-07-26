2a67d5b81cc62fc7ba4b78855c823ef6708d1851
Exanite <exanite78012@gmail.com>
2022-07-26 16:25:03 -0400

    Remove commented code in QuickSortStrategy

--- src/data/algorithms/QuickSortStrategy.ts
+++ src/data/algorithms/QuickSortStrategy.ts
@@ -1,22 +1,16 @@
-import { getFirstPivotIndex, getMedianOf3PivotIndex, getMiddlePivotIndex, PivotIndexSelector } from "@/data/algorithms/misc/QuickSortPivots";
-import { SortingStrategy } from "@/data/interfaces/SortingStrategy";
+import { getFirstPivotIndex, getMedianOf3PivotIndex, PivotIndexSelector } from "@/data/algorithms/misc/QuickSortPivots";
 import { Comparer } from "@/data/comparers/Comparer";
+import { SortingStrategy } from "@/data/interfaces/SortingStrategy";
 
 const partition = <T>(collection: T[], startIndex: number, endIndex: number, comparer: Comparer, pivotSelector: PivotIndexSelector): number => {
   let temp;
   const pivotIndex = pivotSelector(collection, startIndex, endIndex, comparer);
   const pivot = collection[pivotIndex];
 
-  // console.log(`Starting with Start ${startIndex} - End ${endIndex}`);
-  // console.log(JSON.stringify(collection));
-
   temp = collection[startIndex];
   collection[startIndex] = collection[pivotIndex];
   collection[pivotIndex] = temp;
-  
-  // console.log(`Swapped Pivot ${pivotIndex} with Start ${startIndex}`);
-  // console.log(JSON.stringify(collection));
-  
+
   let leftIndex = startIndex + 1;
   let rightIndex = endIndex - 1;
 
@@ -26,20 +20,17 @@ const partition = <T>(collection: T[], startIndex: number, endIndex: number, com
 
       continue;
     }
-    
+
     if (comparer.compare(collection[rightIndex], pivot) >= 0) {
       rightIndex--;
-      
+
       continue;
     }
-    
+
     if (leftIndex < rightIndex) {
       temp = collection[leftIndex];
       collection[leftIndex] = collection[rightIndex];
       collection[rightIndex] = temp;
-
-      // console.log(`Swapped ${leftIndex} with ${rightIndex}`);
-      // console.log(JSON.stringify(collection));
     }
   }
 
@@ -47,11 +38,6 @@ const partition = <T>(collection: T[], startIndex: number, endIndex: number, com
   collection[rightIndex] = collection[startIndex];
   collection[startIndex] = temp;
 
-  // console.log("Swapped pivot into final place");
-  // console.log(`Left ${leftIndex} - Right ${rightIndex}`);
-  // console.log("Ending");
-  // console.log(JSON.stringify(collection));
-  
   return rightIndex;
 };
 
@@ -64,15 +50,12 @@ const quickSort = <T>(collection: T[], startIndex: number, endIndex: number, com
 
   quickSort(collection, startIndex, pivotIndex, comparer, pivotSelector);
   quickSort(collection, pivotIndex + 1, endIndex, comparer, pivotSelector);
-  
+
   return collection;
 };
 
-// const collection = [8,4,2,7,6,5,9,1,0,3]
-// console.log(quickSort(collection, 0, collection.length, new Comparer(x => x), getFirstPivotIndex));
-
 export const QuickSortFirstStrategy: SortingStrategy = {
-  name: "Quick Sort with First Element as Pivot",
+  name: "Quick Sort with First Element Pivot",
   key: "QuickSortFirstStrategy",
   sort<T>(collection: T[], comparer: Comparer): T[] {
     return quickSort(collection, 0, collection.length, comparer, getFirstPivotIndex);
